5d1b13f2ebac2a111ab274ef6ddee2b7923627aa
Matias Leonel Ramirez <matiasleonel.ar>
2024-10-25 21:13:05 -0300

    Encriptador y JWT

---
+++ WebAPI-Log/Custom/Utilidades.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using WebAPI_Log.Models;
+
+namespace WebAPI_Log.Custom
+{
+    public class Utilidades
+    {
+        private readonly IConfiguration _configuration;
+        public Utilidades(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string encriptarSHA256(string texto)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                //Computar el Hash
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(texto));
+
+                //Convertir el array de Bits a String 
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++) 
+                {
+                    builder.Append(bytes[i].ToString("X2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+
+        public string generarJWT(Usuario modelo)
+        {
+            //crear la informacion del usuario para el token
+            var userClaims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, modelo.IdUsuario.ToString()),
+                new Claim(ClaimTypes.Email, modelo.Correo!)
+            };
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:key"]!));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            //Crear detalle del token
+            var jwlConfig = new JwtSecurityToken(
+                claims: userClaims,
+                expires: DateTime.UtcNow.AddMinutes(10),
+                signingCredentials: credentials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(jwlConfig);
+        }
+    }
+}
---
+++ WebAPI-Log/Models/DbpruebaContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI_Log.Models;
+
+public partial class DbpruebaContext : DbContext
+{
+    public DbpruebaContext()
+    {
+    }
+
+    public DbpruebaContext(DbContextOptions<DbpruebaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.IdUsuario).HasName("PK__Usuario__5B65BF97B07C57D5");
+
+            entity.ToTable("Usuario");
+
+            entity.Property(e => e.Clave)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Correo)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
